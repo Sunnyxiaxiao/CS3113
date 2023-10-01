81ef00f68afe419e636cfb48fc10d8bb99b086b7
邹雨柔 <zouyurou@yawaras-MBP.ht.home>
2023-09-30 21:19:51 -0400

    hw1

---
+++ SDL_HW1/SDL_HW1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_HW1
+//
+//  Created by 邹雨柔 on 9/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
